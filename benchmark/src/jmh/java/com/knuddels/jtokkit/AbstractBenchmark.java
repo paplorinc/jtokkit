e98ef3150ccacf41c2bf61ca7c3bc126c1b69003
Lőrinc <lorinc.pap@gmail.com>
2023-11-10 15:26:36 +0100

    Add before-after SingleThreadedBenchmark notes

--- benchmark/src/jmh/java/com/knuddels/jtokkit/AbstractBenchmark.java
+++ benchmark/src/jmh/java/com/knuddels/jtokkit/AbstractBenchmark.java
@@ -9,29 +9,9 @@ import static java.util.stream.Collectors.toList;
 
 public abstract class AbstractBenchmark {
 
-//    @Benchmark
-    public int benchmarkCl100kBaseTokenCountReference(BenchmarkingState state) {
-        var result = encodeAll(state.cl100kBase, state.fileContents).stream().mapToInt(List::size).sum();
-        if (result != state.expectedFileContentsCl100kBaseTokenCount) {
-            throw new RuntimeException(String.format("Wrong token count: %d != %d", result, state.expectedFileContentsCl100kBaseTokenCount));
-        }
-        return result;
-    }
-
-
     @Benchmark
     public int benchmarkCl100kBaseTokenCount(BenchmarkingState state) {
         var result = countTokens(state.cl100kBase, state.fileContents);
-        if (result != state.expectedFileContentsCl100kBaseTokenCount) {
-            throw new RuntimeException(String.format("Wrong token count: %d != %d", result, state.expectedFileContentsCl100kBaseTokenCount));
-        }
-        return result;
-    }
-
-//    @Benchmark
-    public int benchmarkCl100kBaseTokenCountWithInit(BenchmarkingState state) {
-        var encoding = EncodingFactory.cl100kBase();
-        var result = countTokens(encoding, state.fileContents);
         if (result != state.expectedFileContentsCl100kBaseTokenCount) {
             throw new RuntimeException(String.format("Wrong token count: %d != %d", result, state.expectedFileContentsCl100kBaseTokenCount));
         }
--- benchmark/src/jmh/java/com/knuddels/jtokkit/SingleThreadedBenchmark.java
+++ benchmark/src/jmh/java/com/knuddels/jtokkit/SingleThreadedBenchmark.java
@@ -4,6 +4,13 @@ import com.knuddels.jtokkit.api.Encoding;
 import java.util.List;
 import java.util.stream.Collectors;
 
+// Before:
+//Benchmark                                              (dataFolderPath)  Mode  Cnt  Score   Error  Units
+//SingleThreadedBenchmark.benchmarkCl100kBaseTokenCount              data    ss   10  3.828 ± 0.190   s/op
+//
+// After:
+//Benchmark                                              (dataFolderPath)  Mode  Cnt  Score   Error  Units
+//SingleThreadedBenchmark.benchmarkCl100kBaseTokenCount              data    ss   10  2.864 ± 0.174   s/op
 public class SingleThreadedBenchmark extends AbstractBenchmark {
 
 }
